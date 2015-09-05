d13665af78b1f5abcb8b6347683461a5629ef064
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-09-05 16:58:16 +0300

    Adds NR Monitoring in production.setup.sh

--- production.setup.sh
+++ production.setup.sh
@@ -58,3 +58,11 @@ sudo -u huginn -H chmod o-rwx .env
 
 # install gems
 sudo -u huginn -H bundle install --deployment --without development test
+
+# install new relic monitoring
+echo deb http://apt.newrelic.com/debian/ newrelic non-free >> /etc/apt/sources.list.d/newrelic.list
+wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
+apt-get update
+apt-get install newrelic-sysmond
+nrsysmond-config --set license_key=
+/etc/init.d/newrelic-sysmond start
